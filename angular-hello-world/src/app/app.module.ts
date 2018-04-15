1a3e5e80b80bb80dd177e449fe1c2466506f7a73
heidilaursen <heidilaursen@protonmail.com>
2018-04-15 15:49:36 -0700

    adding new component

--- angular-hello-world/src/app/app.module.ts
+++ angular-hello-world/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { VendorsComponent } from './vendors/vendors.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VendorsComponent
   ],
   imports: [
     BrowserModule
